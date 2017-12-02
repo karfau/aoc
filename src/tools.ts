9801a56d46abb5be71a70b5ca60ea172b52b8f17
Christian Bewernitz <coder@karfau.de>
2017-12-03 00:35:37 +0100

    ts-node, get-stdin, lodash, run method and script

--- src/tools.ts
+++ src/tools.ts
@@ -1,3 +1,7 @@
 export const lines = (s: string) => s.split(/[\n\r]/).map(line => line.trim()).filter(line => line.length > 0);
 
 export const numbers = (s: string, delim: string | RegExp = '') => s.split(delim).map(c => parseInt(c, 10));
+
+export const run = (fun: Function) => {
+  require('get-stdin')().then(fun).then(console.log);
+};
