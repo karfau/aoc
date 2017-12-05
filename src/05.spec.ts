6039063b5f60a7d01ad12c29e3e0528e647e26ec
Christian Bewernitz <coder@karfau.de>
2017-12-05 09:05:04 +0100

    day 5 star 2

--- src/05.spec.ts
+++ src/05.spec.ts
@@ -1,20 +1,6 @@
-import {findExit, jump} from './05';
+import {findExit, jump, jump3} from './05';
 
-describe('day 4', function () {
-  describe('jump()', function () {
-    it(`"should only increase for 0`, function () {
-      const position = 0;
-      const instructions = [0];
-      expect(jump(instructions, position)).toBe(0);
-      expect(instructions).toEqual([1]);
-    });
-    it(`"should increase and jump for 1`, function () {
-      const position = 0;
-      const instructions = [1];
-      expect(jump(instructions, position)).toBe(1);
-      expect(instructions).toEqual([2]);
-    });
-  });
+describe('day 5', function () {
   describe('jump()', function () {
     it(`"should only increase for 0`, function () {
       const position = 0;
@@ -39,6 +25,9 @@ describe('day 4', function () {
       it(`"should need ${steps} steps for [${input.join()}]`, function () {
         expect(findExit(input)).toBe(steps);
       });
+    });
+    it(`"should need 10 steps for star 2`, function () {
+      expect(findExit([0, 3,  0,  1,-3], jump3)).toBe(10);
     });
   });
 });
--- src/05.ts
+++ src/05.ts
@@ -1,23 +1,27 @@
-import {uniq} from 'lodash';
 import {runWithStdIn, splitLines} from './tools';
 
 export const jump = (instructions: number[], position: number): number => {
   return position + instructions[position]++
 };
 
-export const findExit = (instructions: number[]): number => {
+export const jump3 = (instructions: number[], position: number): number => {
+  return instructions[position] >= 3 ? position + instructions[position]-- : position + instructions[position]++
+};
+
+export const findExit = (instructions: number[], doStep: typeof jump = jump): number => {
   let steps = 0, pos = 0;
-  // const step = jump.bind(null, instructions);
   while (pos < instructions.length) {
-    pos = jump(instructions, pos);
+    pos = doStep(instructions, pos);
     steps++;
   }
   return steps;
 };
 
 export const main = (input: string) => {
-
-  return findExit(splitLines(input).map(l => parseInt(l, 10)));
+  return [
+    findExit(splitLines(input).map(l => parseInt(l, 10))),
+    findExit(splitLines(input).map(l => parseInt(l, 10)), jump3)
+  ];
 };
 
 if (require.main === module) runWithStdIn(main);
