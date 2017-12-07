b6d3cde7e2df35aeca703efa7208dcd469a585e3
Christian Bewernitz <coder@karfau.de>
2017-12-07 09:46:35 +0100

    day 7 star 2
    (getting the solution still requires parsing of the output)

--- src/07.spec.ts
+++ src/07.spec.ts
@@ -1,11 +1,7 @@
 import {splitLines} from './tools';
-import {findRoot} from './07';
+import {findRoot, Sums, sums, tasks, Tower, tower} from './07';
 
-describe('day 7', function () {
-  describe('findRoot()', function () {
-
-    it(`"should find tknk`, function () {
-      expect(findRoot(splitLines(`
+const INPUT = `
 pbga (66)
 xhth (57)
 ebii (61)
@@ -19,7 +15,51 @@ jptl (61)
 ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)
-      `))).toEqual('tknk');
+`;
+
+describe('day 7', function () {
+  describe('findRoot()', function () {
+    it(`"should find tknk`, function () {
+      expect(findRoot(splitLines(INPUT))).toEqual('tknk');
+    });
+  });
+  describe('tree()', function () {
+    it(`"should create tree for`, function () {
+      expect(tower(splitLines(INPUT))).toMatchSnapshot();
+    });
+  });
+  describe('tasks()', function () {
+    it(`"should create tasks for one level`, function () {
+      expect(tasks({root:{w: NaN,c:['a', 'b']}}, 'root'))
+        .toEqual([['root'], ['a', 'b']]);
+    });
+    it(`"should create tasks for two levels`, function () {
+      expect(tasks(
+        {
+          root:{w: NaN,c:['a1', 'b1']},
+          a1:{w: NaN,c:['c2']}
+        }, 'root')).toEqual([['root'], ['a1', 'b1'], ['c2']]);
+    });
+    it(`"should create tasks for est input`, function () {
+      expect(tasks(tower(splitLines(INPUT)), 'tknk')).toMatchSnapshot();
+    });
+  });
+  describe('sums()', function () {
+    let t: Tower;
+    beforeAll(() => {
+      t = tower(splitLines(INPUT));
+    });
+    it(`"should create sum for single`, function () {
+      const expected: Sums = {jptl:[61, []]};
+      expect(sums(t, tasks(t, 'jptl'))).toEqual(expected);
+    });
+    it(`"should create sums for ugml`, function () {
+      const expected: Sums = {ugml: [251,[61, 61, 61]], gyxo: [61, []], ebii:[61, []], jptl:[61, []]};
+      expect(sums(t, tasks(t, 'ugml'))).toEqual(expected);
+    });
+    it(`"should create sums for test input`, function () {
+      const expected: Sums = {ugml: [251,[61, 61, 61]], gyxo: [61, []], ebii:[61, []], jptl:[61, []]};
+      expect(sums(t, tasks(t, 'tknk'))['tknk']).toEqual([778, [251, 243, 243]]);
     });
   });
 });
--- src/07.ts
+++ src/07.ts
@@ -1,18 +1,65 @@
 import {runWithStdIn, splitLines} from './tools';
-import {difference} from 'lodash'
+import {difference, flatten, sum, uniq} from 'lodash';
+
+export type Program = {
+  w: number;
+  c: string[];
+  s?: number;
+}
+
+export type Tower = {
+  [name: string]: Program
+}
 
 export function findRoot(lines: ReadonlyArray<string>): string {
   const children: string[] = [];
   return difference(lines.map(line => {
-    const [head, ...below] = line.split(/[ (\d)->,]+/);
+    const [head, ...below] = line.split(/[ (\d)\->,]+/);
     children.push(...below);
     return head;
   }), children)[0];
 }
 
+export const tower = (lines: ReadonlyArray<string>): Tower => {
+  return lines.reduce((tower, line) => {
+    const [head, w, ...c] = line.split(/[\s()\->,]+/);
+    tower[head as string] = {w: parseInt(w), c: c.length === 1 && c[0] === '' ? [] : c};
+    return tower;
+  }, {} as Tower);
+};
+
+export const tasks = (t: Tower, root: string): string[][] => {
+  const result: string[][] = [];
+  let current: string[] = [root];
+  do {
+    const next = flatten(current.map(c => c in t ? t[c].c : []));
+    result.push(current);
+    current = next;
+  } while (current.length > 0);
+  return result;
+};
+
+export type Sums = {[key: string]: [number, number[]]}
+
+export const sums = (t:Tower, tasks: string[][]): Sums => {
+  return [...tasks].reverse().reduce((result, tasklevel, lvl) => {
+    tasklevel.reduce((r, task) => {
+      const cw = t[task].c.map(c => r[c][0]);
+      r[task] = [sum(cw) + t[task].w, cw];
+      if (uniq(cw).length>1){
+        console.log(tasks.length - lvl, cw, t[task].c.map(c => [c, t[c].w, ...r[c]]));
+      }
+      return r
+    }, result);
+    return result;
+  }, {} as Sums)
+};
+
 export const main = (input: string) => {
   const lines = splitLines(input);
-  return findRoot(lines);
+  const root = findRoot(lines);
+  const t = tower(lines);
+  sums(t, tasks(t, root))
 };
 
 if (require.main === module) runWithStdIn(main);
