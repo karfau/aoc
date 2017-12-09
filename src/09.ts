b81f9d5521f6946aca1084f682cc7f54ec0ca830
Christian Bewernitz <coder@karfau.de>
2017-12-09 22:00:25 +0100

    some cleanup and formatting

--- src/09.ts
+++ src/09.ts
@@ -1,6 +1,5 @@
 import {runWithStdIn} from './tools';
 
-
 const CANCELED_CHARS = /!./g;
 const GARBAGE = /<.*?>/;
 const ALL_GARBAGE = new RegExp(GARBAGE.source, 'g');
@@ -10,16 +9,16 @@ export function removeGarbage(input: string): string {
   return input.replace(CANCELED_CHARS, '').replace(ALL_GARBAGE, '');
 }
 
-export function measureGarbage(input: string): number {
-  const reduceAndMeasureGarbage = (amount: number, current: string): number => {
-    if (!GARBAGE.test(current)) {
-      return amount
-    }
-    const next = current.replace(GARBAGE, '');
-    const charsInCurrentGarbage = (current.length - next.length - START_AND_END);
-    return reduceAndMeasureGarbage(amount + charsInCurrentGarbage, next)
-  };
+const reduceAndMeasureGarbage = (amount: number, current: string): number => {
+  if (!GARBAGE.test(current)) {
+    return amount
+  }
+  const next = current.replace(GARBAGE, '');
+  const currentAmount = (current.length - next.length - START_AND_END);
+  return reduceAndMeasureGarbage(amount + currentAmount, next)
+};
 
+export function measureGarbage(input: string): number {
   return reduceAndMeasureGarbage(0, input.replace(CANCELED_CHARS, ''));
 }
 
@@ -34,16 +33,12 @@ type GroupScore = {
 };
 
 export function scoreGroups(input: string): number {
-
   return input.split('').reduce<GroupScore>(
     ({level, total}, char) => {
       switch (char) {
-        case '{':
-          return {level: level+1, total: total+level};
-        case '}':
-          return {level: level-1, total};
-        default:
-          return {level, total};
+        case '{': return {level: level+1, total: total+level};
+        case '}': return {level: level-1, total};
+        default: return {level, total};
       }
     },
     {level: 1, total: 0}
@@ -51,7 +46,10 @@ export function scoreGroups(input: string): number {
 }
 
 export const main = (input: string) => {
-  return [scoreGroups(removeGarbage(input)), measureGarbage(input)];
+  return [
+    scoreGroups(removeGarbage(input)),
+    measureGarbage(input)
+  ];
 };
 
 if (require.main === module) runWithStdIn(main);
