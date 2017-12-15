be6920e9e81f2ba74813e77c9d40e4cddac8cf25
Christian Bewernitz <coder@karfau.de>
2017-12-15 18:29:37 +0100

    day 15 star 2

--- src/15.spec.ts
+++ src/15.spec.ts
@@ -1,4 +1,4 @@
-import {countMatches, GEN_A, GEN_B, generate, matchBinary} from './15';
+import {CHECK_4, CHECK_8, countMatches, GEN_A, GEN_B, generate, matchBinary} from './15';
 
 const START_A = 65;
 const START_B = 8921;
@@ -24,6 +24,24 @@ describe('day 15', () => {
         expect(generate(prev, factor)).toEqual(result);
       });
     });
+
+    [
+      [START_A, GEN_A, 1352636452],
+      [1352636452, GEN_A, 1992081072],
+      [1992081072, GEN_A, 530830436],
+      [530830436, GEN_A, 1980017072],
+      [1980017072, GEN_A, 740335192],
+
+      [START_B, GEN_B, 1233683848],
+      [1233683848, GEN_B, 862516352],
+      [862516352, GEN_B, 1159784568],
+      [1159784568, GEN_B, 1616057672],
+      [1616057672, GEN_B, 412269392],
+    ].forEach(([prev, factor, result]: [number, number, number]) => {
+      it(`should generate ${result} from ${prev} and factor ${factor} with predicate`, () => {
+        expect(generate(prev, factor, factor === GEN_A ? CHECK_4 : CHECK_8)).toEqual(result);
+      });
+    });
   });
 
   describe('matchBinary()', () => {
@@ -32,9 +50,12 @@ describe('day 15', () => {
     });
   });
 
-  describe.skip('countMatches()', () => {
+  describe('countMatches()', () => {
     it('should match from example', () => {
       expect(countMatches(START_A, START_B)).toBe(588)
+    });
+    it('should match from example with predicate', () => {
+      expect(countMatches(START_A, START_B, 5000000, true)).toBe(309)
     });
   });
 });
--- src/15.ts
+++ src/15.ts
@@ -1,12 +1,19 @@
-import {lineToNumbers, runWithStdIn, splitLines} from './tools';
 import {padStart} from 'lodash'
 
 export const GEN_A = 16807;
 export const GEN_B = 48271;
 export const DIVIDER = 2147483647;
 
-export const generate = (start: number, factor: number): number => {
-  return (start * factor) % DIVIDER;
+export type Check = (n: number) => boolean;
+export const CHECK_4: Check = n => n % 4 === 0;
+export const CHECK_8: Check = n => n % 8 === 0;
+
+export const generate = (start: number, factor: number, check?: Check): number => {
+  let next = start;
+  do {
+    next = (next * factor) % DIVIDER;
+  } while (check && !check(next));
+  return next;
 };
 
 
@@ -17,11 +24,11 @@ export const matchBinary = (a: number, b: number): boolean => {
   return toBinary(a, MATCH_LENGTH).substr(-MATCH_LENGTH) === toBinary(b, MATCH_LENGTH).substr(-MATCH_LENGTH)
 };
 
-export const countMatches = (a: number, b: number): number => {
+export const countMatches = (a: number, b: number, numChecks = 40000000, withCheck = false): number => {
   let result = 0, i = 0, currentA = a, currentB = b;
-  while (i++ < 40000000) {
-    currentA = generate(currentA, GEN_A);
-    currentB = generate(currentB, GEN_B);
+  while (i++ < numChecks) {
+    currentA = generate(currentA, GEN_A, withCheck ? CHECK_4 : undefined);
+    currentB = generate(currentB, GEN_B, withCheck ? CHECK_8 : undefined);
     if (matchBinary(currentA, currentB)) {
       result++;
     }
@@ -29,9 +36,8 @@ export const countMatches = (a: number, b: number): number => {
   return result;
 };
 
-export const main = (input: string) => {
-  // const numbers = lineToNumbers(splitLines(input)[0], /\s+/);
-  return countMatches(722, 354);
+export const main = (start_a: number, start_b: number) => {
+  return countMatches(start_a, start_b, 5 * 1000 * 1000, true);
 };
 
-if (require.main === module) runWithStdIn(main);
+if (require.main === module) console.log(main(722, 354));
