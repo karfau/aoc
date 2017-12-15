ba3931fca220efa03f162e18df7ae1ab0991e132
Christian Bewernitz <coder@karfau.de>
2017-12-15 14:35:09 +0100

    first example test works

--- src/15.spec.ts
+++ src/15.spec.ts
@@ -1,9 +1,19 @@
-import {generate} from './15';
+import {GEN_A, GEN_B, generate} from './15';
 
 describe('day 15', () => {
   describe('generate()', () => {
     [
-      [0, 0, 0],
+      [65, GEN_A, 1092455],
+      [1092455, GEN_A, 1181022009],
+      [1181022009, GEN_A, 245556042],
+      [245556042, GEN_A, 1744312007],
+      [1744312007, GEN_A, 1352636452],
+
+      [8921, GEN_B, 430625591],
+      [430625591, GEN_B, 1233683848],
+      [1233683848, GEN_B, 1431495498],
+      [1431495498, GEN_B, 137874439],
+      [137874439, GEN_B, 285222916],
     ].forEach(([prev, factor, result]: [number, number, number]) => {
       it(`should generate ${result} from ${prev} and factor ${factor}`, () => {
         expect(generate(prev, factor)).toEqual(result);
--- src/15.ts
+++ src/15.ts
@@ -1,8 +1,12 @@
 import {lineToNumbers, runWithStdIn, splitLines} from './tools';
 import {uniq} from 'lodash'
 
+export const GEN_A = 16807;
+export const GEN_B = 48271;
+export const DIVIDER = 2147483647;
+
 export const generate = (start: number, factor: number): number => {
-  return start;
+  return (start * factor) % DIVIDER;
 };
 
 export const main = (input: string) => {
