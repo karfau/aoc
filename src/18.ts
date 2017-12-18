2fb6b85b4b9ef27b88cc6af57f4f26877efe902d
Christian Bewernitz <coder@karfau.de>
2017-12-18 19:24:42 +0100

    extracted valueFor

--- src/18.ts
+++ src/18.ts
@@ -1,15 +1,19 @@
 import {lineToNumbers, runWithStdIn, splitLines} from './tools';
-import {uniq} from 'lodash';
+import {curry} from 'lodash';
+
+export const valueFor = (state: any, keyOrValue: string): number =>
+  keyOrValue in state ? state[keyOrValue] : parseInt(keyOrValue, 10);
 
 export function instruction(state: any, input: string): any {
+  const val = (kv: string) => valueFor(state, kv);
   const [cmd, first, second] = input.split(/\s+/);
   const updated: any = {_move: 1};
   switch (cmd) {
     case 'snd':
-      updated._snd = first in state ? state[first] : parseInt(first, 10);
+      updated._snd = val(first);
       break;
     case 'set':
-      updated[first] = second in state ? state[second] : parseInt(second, 10);
+      updated[first] = val(second);
       break;
   }
 
