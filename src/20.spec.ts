7cba209fd51b77003cbc230d55c6fb5e0207bc25
Christian Bewernitz <coder@karfau.de>
2017-12-20 22:59:01 +0100

    code solution for star 1 ?
    Particles are a class again, and after lots of iterations
    I tried to implement the logic that I used to "find" first solution

--- src/20.spec.ts
+++ src/20.spec.ts
@@ -1,37 +1,78 @@
-import {main, parseParticles, particle, Particle, tick, Vector} from './20';
+import {findClosest, parseParticles, Particle, removeCollisions, tick, Vector} from './20';
 
-const EXAMPLE = `
+const EXAMPLE_1 = parseParticles(`
+p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>
+p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>
+`);
+
+const EXAMPLE_2 = parseParticles(`
 p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>
 p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>
 p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>
 p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>
-`;
+`);
+
+const EXAMPLE_STILL_DIST = parseParticles(`
+p=<-6,0,0>, v=<0,0,0>, a=< 0,0,0>
+p=<-4,0,0>, v=<0,0,0>, a=< 0,0,0>
+p=<-2,0,0>, v=<0,0,0>, a=< 0,0,0>
+p=< 3,0,0>, v=<0,0,0>, a=< 0,0,0>
+`);
+
+const EXAMPLE_SAME = parseParticles(`
+p=<4,0,0>, v=<1,0,0>, a=< 0,0,0>
+p=<0,4,0>, v=<0,1,0>, a=< 0,0,0>
+p=<0,0,4>, v=<0,0,1>, a=< 0,0,0>
+`);
+
+const EXAMPLE_STILL = parseParticles(`
+p=<4,0,0>, v=<0,0,0>, a=< 0,0,0>
+p=<0,4,0>, v=<0,0,0>, a=< 0,0,0>
+p=<0,0,4>, v=<0,0,0>, a=< 0,0,0>
+`);
 
 describe('day 20', () => {
   describe('particle()', () => {
     [
-      [particle(0,0,0, 1,1,1, 1,1,1), 0 , [0,0,0]],
-      [particle(0,0,0, 1,1,1, 2,2,2), 1 , [3,3,3]],
-      [particle(0,0,0, 1,1,1, 2,2,2), 2 , [8,8,8]],
-      [particle(-10,-10,-10, 1,1,1, 2,2,2), 2 , [-2,-2,-2]],
+      [new Particle(0,0,0, 1,1,1, 1,1,1), 0 , [0,0,0]],
+      [new Particle(0,0,0, 1,1,1, 2,2,2), 1 , [3,3,3]],
+      [new Particle(0,0,0, 1,1,1, 2,2,2), 2 , [8,8,8]],
+      [new Particle(-10,-10,-10, 1,1,1, 2,2,2), 2 , [-2,-2,-2]],
     ].forEach(([p, t, result]: [Particle, number, Vector]) => {
       it(`${p}(${t}) should return [${result}]`, () => {
-        expect(p.t(t)).toEqual(result);
+        expect(p.pos(t)).toEqual(result);
       });
     });
   });
   
   describe('tick()', () => {
     it(`should work for example`, () => {
-      const particles = parseParticles(EXAMPLE);
-      console.log(particles.map(p => p.toString()));
-      expect(tick(particles, 0).length).toEqual(4);
-      expect(tick(particles, 2).length).toEqual(1);
+      expect(tick(EXAMPLE_2, 0).length).toEqual(4);
+      expect(tick(EXAMPLE_2, 2).length).toEqual(1);
     });
   });
-  describe('main()', () => {
-    it(`should work for example`, () => {
-      expect(main(EXAMPLE)).toEqual(1);
+
+  describe('removeCollisions()', () => {
+    it(`should work for example 1`, () => {
+      expect(removeCollisions(EXAMPLE_1)).toBe(2);
+    });
+    it(`should work for example 2`, () => {
+      expect(removeCollisions(EXAMPLE_2)).toBe(1);
+    });
+  });
+
+  describe('findClosest()', () => {
+
+    [
+      ['example 1', EXAMPLE_1, 0],
+      ['example 2', EXAMPLE_2, 3],
+      ['no movement but different distances', EXAMPLE_STILL_DIST, 2],
+      ['same movement and distances all the time', EXAMPLE_SAME, -1],
+      ['no movement same distances', EXAMPLE_STILL, -1],
+    ].forEach(([what, particles, result]: [string, ReadonlyArray<Particle>, number]) => {
+      it(`${what} should return ${result}`, () => {
+        expect(findClosest(particles)).toBe(result);
+      });
     });
   });
 });
--- src/20.ts
+++ src/20.ts
@@ -1,41 +1,107 @@
-import {lineToNumbers, runWithStdIn, splitLines} from './tools';
-import {countBy} from 'lodash';
+import {lineToNumbers, Numbers, runWithStdIn, splitLines} from './tools';
+import {countBy, intersection} from 'lodash';
 
-export type Vector = [number, number, number];
-export type Particle = {
-  t: (t: number) => Vector;
-  toString: () => string;
-}
+export type Vector = Readonly<[number, number, number]>;
 
 export const calc = (t: number, p: number, v: number, a: number) =>
   t === 0 ? p : p + v * t + a * (t * (t + 1)) / 2;
 
-export const particle = (...values: number[]): Particle => {
-  const [px, py, pz, vx, vy, vz, ax, ay, az] = values;
-  return {
-    t: t => [
+export class Particle {
+
+  private readonly _pos: Vector;
+  readonly acc: Vector;
+  readonly vel: Vector;
+  readonly pos: (t: number) => Vector;
+
+  constructor(...values: number[]) {
+    const [px, py, pz, vx, vy, vz, ax, ay, az] = values;
+    this._pos = [px, py, pz];
+    this.acc = [ax, ay, az];
+    this.vel = [vx, vy, vz];
+    this.pos = (t: number) => [
       calc(t, px, vx, ax),
       calc(t, py, vy, ay),
       calc(t, pz, vz, az)
-    ],
-    toString: () => `(p:${[px, py, pz]}; v:${[vx, vy, vz]}; a:${[ax, ay, az]})`
-  };
-};
+    ];
+  }
+
+  toString() {
+    return `(p:${this._pos}; v:${this.vel}; a:${this.acc})`;
+  }
+}
 
 export function tick(input: ReadonlyArray<Particle>, t: number): ReadonlyArray<Particle> {
-  const pos = input.map(p => p.t(t));
+  const pos = input.map(p => p.pos(t));
   const grouped = countBy(pos.map(p => p.join(',')));
   return input.filter((it, i) => grouped[pos[i].join(',')] === 1);
 }
 
+const TESTS = 20;
+
+const absolute = (pos: Vector) => pos.map(Math.abs).reduce((sum, v) => sum + v, 0);
+
+type Min = [number, Numbers];
+
+const min = (list: Numbers): Min => {
+  const value = Math.min(...list);
+  const indexes = list.map((v, i) => i).filter(i => list[i] === value);
+  return [value, indexes];
+};
+
+const returnSingleOrCall = (test: Numbers, call: () => number): number =>
+  test.length === 1 ? test[0] : call();
+
+export const findClosest = (particles: ReadonlyArray<Particle>): number => {
+
+  // find the particles with the lowest "absolute acceleration"
+  const acc: Numbers = particles.map(p => absolute(p.acc));
+
+  /** slowest contains indexes from particles */
+  const slowest = min(acc)[1];
+
+  if (slowest.length === 1) {
+    return slowest[0];
+  }
+
+  const iteration = (
+    lastDistances: Numbers = [], lastIntersection: Numbers = [], repIntersect: number = 0, t: number = 1
+  ): number => {
+    const distances = slowest.map(i => absolute(particles[i].pos(t)));
+    const differences = distances.map(
+      (distance, i) => i < lastDistances.length ? distance - lastDistances[i] : -1
+    );
+    const closest = min(distances)[1];
+    const [minDiff, moved] = min(differences);
+    const intersec = intersection(closest, moved).sort((a, b) => a - b);
+    const repeated = intersec.length > 1 && intersec.join(',') === lastIntersection.join(',') ? repIntersect + 1 : 0;
+    console.log(t, distances, differences, lastIntersection);
+    if (repeated >= TESTS){
+      console.warn('intersections didn\'t change');
+      return -1;
+    }
+
+    if (differences.every(d => d === 0)){
+      return returnSingleOrCall(intersec, () => -1);
+    }
+
+    const next = () => iteration(distances, intersec, repeated, t+1);
+    if (minDiff > 0) {// all particles move away from zero
+      return returnSingleOrCall(intersec, next)
+    }
+
+    return next();
+  };
+
+  return returnSingleOrCall(slowest, iteration);
+};
+
 export const parseParticles = (input: string): ReadonlyArray<Particle> =>
   splitLines(input).map(
-    line => particle(...lineToNumbers(line.replace(/[pva=<>\s]/g, ''), ','))
+    line => new Particle(...lineToNumbers(line.replace(/[pva=<>\s]/g, ''), ','))
   );
 
-const TESTS = 100;
-export const main = (input: string) => {
-  let current = parseParticles(input);
+export const removeCollisions = (particles: ReadonlyArray<Particle>): number => {
+  let current = particles;
   let amount = current.length;
   let t = 0, i = TESTS;
   while (i > 0) {
@@ -44,6 +110,14 @@ export const main = (input: string) => {
     amount = current.length;
   }
   return amount;
+};
+
+export const main = (input: string) => {
+  const particles = parseParticles(input);
+  return [
+    findClosest(particles),
+    removeCollisions(particles)
+  ];
 };
 
 if (require.main === module) runWithStdIn(main);
