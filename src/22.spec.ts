157e0a8c011aab725a5982f760e800c74b098153
Christian Bewernitz <coder@karfau.de>
2017-12-23 02:12:37 +0100

    day 22 star 1

--- src/22.spec.ts
+++ src/22.spec.ts
@@ -1,5 +1,6 @@
 import { splitLines} from './tools';
-import {Direction, DirectionKey, parseGrid, turn} from './22';
+import {burst, countInfectingBursts, Direction, DirectionKey, Infection, parseGrid, turn} from './22';
+import {range} from 'lodash';
 
 
 const EXAMPLE: ReadonlyArray<string> = splitLines(`
@@ -8,12 +9,60 @@ const EXAMPLE: ReadonlyArray<string> = splitLines(`
 ...
 `);
 
+const EXAMPLE_BIG: ReadonlyArray<string> = splitLines(`
+.........
+.........
+.........
+.....#...
+...#.....
+.........
+.........
+.........
+.........
+`);
+
+const ONE: ReadonlyArray<string> = splitLines(`
+.........
+.........
+.........
+.....#...
+...##....
+.........
+.........
+.........
+.........
+`);
+
+const TWO: ReadonlyArray<string> = splitLines(`
+.........
+.........
+.........
+.....#...
+...=#....
+.........
+.........
+.........
+.........
+`);
+
 const EXAMPLE_2: ReadonlyArray<string> = splitLines(`
 #...#
 .....
 .....
 .....
 #...#
+`);
+
+const SEVEN: ReadonlyArray<string> = splitLines(`
+.........
+.........
+.........
+..#=.#...
+..###....
+.........
+.........
+.........
+.........
 `);
 
 describe('day 22', () => {
@@ -28,36 +77,54 @@ describe('day 22', () => {
       ['up', false, 'right'],
       ['right', false, 'down'],
       ['down', false, 'left'],
-    ].forEach(([dir, left, next]: [DirectionKey, boolean, DirectionKey) => {
+    ].forEach(([dir, left, next]: [DirectionKey, boolean, DirectionKey]) => {
       it(`should turn ${left ? 'left': 'right'} from ${dir} to ${next}`, () => {
         expect(turn(Direction[dir], left)).toEqual(Direction[next]);
       });
     });
   });
-/*
-  describe('parse()', () => {
-    [
-      [[0, 5], 'right', 'down'],
-      [[5, 5], 'down', 'right'],
-      [[5, 8], 'right', 'up'],
-      [[1, 8], 'up', 'right'],
-      [[1, 11], 'right', 'down'],
-      [[5, 11], 'down', 'right'],
-      [[5, 14], 'right', 'up'],
-      [[3, 14], 'up', 'left'],
-    ].forEach(([start, from, to]: [Pair<number>, DirectionKey, DirectionKey]) => {
-      it(`should turn from ${from} to ${to} at [${start.join()}]`, () => {
-        expect(turn(EXAMPLE, start, Direction[from])).toEqual(Direction[to]);
-      });
-    });
-  });
-*/
+
   describe('parseGrid()', () => {
     it('should work for example', () => {
       expect(parseGrid(EXAMPLE)).toEqual({'-1,0': true,'1,-1': true})
     });
     it('should work for example 2', () => {
       expect(parseGrid(EXAMPLE_2)).toEqual({'-2,-2': true,'2,-2': true,'2,2': true,'-2,2': true})
+    });
+    it('should work for example with false', () => {
+      expect(parseGrid(TWO)).toEqual({'0,0': true,'1,-1': true,'-1,0': false})
+    });
+    it('should work for small and big example', () => {
+      expect(parseGrid(EXAMPLE)).toEqual(parseGrid(EXAMPLE_BIG))
+    });
+  });
+
+  describe('burst()', () => {
+    it('should work for example', () => {
+      const initial = parseGrid(EXAMPLE);
+      const infection: Infection = {
+        grid: initial,
+        position: [0,0],
+        direction: Direction.up
+      };
+      const steps: Infection[] = [burst(infection)];
+      range(1,7).forEach(i => steps.push(burst(steps[i - 1])));
+      const [first, second, third, fourth, fifth, sixth, seventh] = steps;
+      expect(first).toEqual({grid: parseGrid(ONE), position: [-1,0], direction: Direction.left});
+      expect(second).toEqual({grid: parseGrid(TWO), position: [-1,-1], direction: Direction.up});
+      expect(seventh.grid).toEqual(parseGrid(SEVEN));
+    });
+  });
+
+  describe('countInfectingBursts()', () => {
+    [
+      [7, 5],
+      [70, 41],
+      [10000, 5587],
+    ].forEach(([iterations, expected]: [number, number]) => {
+      it(`${iterations} iterations should cause ${expected} infections`, () => {
+        expect(countInfectingBursts(parseGrid(EXAMPLE), iterations)).toBe(expected)
+      });
     });
   });
 });
--- src/22.ts
+++ src/22.ts
@@ -1,62 +1,90 @@
 import {Lines} from './21';
 import {lineToNumbers, Pair, runWithStdIn, splitLines} from './tools';
-import {values} from 'lodash';
 
-export enum Direction {up = '-1,0', left = '0,-1', down = '1,0', right = '0,1'}
+export enum Direction {up = '0,-1', left = '-1,0', down = '0,1', right = '1,0'}
 
 export type DirectionKey = keyof typeof Direction;
 export const TURN_LEFT: ReadonlyArray<Direction> = [Direction.up, Direction.left, Direction.down, Direction.right];
 
-export type Grid = Readonly<{ [key: string]: boolean }>;
+export const turn = (from: Direction, left: boolean): Direction => {
+  const current = TURN_LEFT.indexOf(from);
+  const next = left ?
+    (current + 1) % TURN_LEFT.length :
+    current > 0 ? current - 1 : TURN_LEFT.length - 1;
+  return TURN_LEFT[next];
+};
 
 export const doStep = (dir: Direction, line: number, col: number): Pair<number> => {
   const [ml, mc] = lineToNumbers(dir, ',');
   return [line + ml, col + mc];
 };
 
+export type Grid = Readonly<{ [key: string]: boolean }>;
+
+export type Infection = {
+  grid: Grid;
+  direction: Direction;
+  position: Pair<number>;
+}
+
+export const burst = ({position, direction, grid}: Infection): Infection => {
+  const [x, y] = position;
+  const pos$ = position.join(',');
+  const isInfected = grid[pos$];
+  const nextDirection = turn(direction, !isInfected);
+  const nextPosition = doStep(nextDirection, x, y);
+  return {
+    direction: nextDirection,
+    position: nextPosition,
+    grid: {
+      ...grid,
+      [pos$]: !isInfected
+    }
+  };
+};
+
 export const parseGrid = (lines: Lines): Grid => {
   const translate = Math.floor(lines.length/2);
-  return lines
-    .reduce((infected, line, li) => {
-      line.split('').forEach((c, ci) => {
-        if (c === '#') {
-          infected.push(`${ci - translate},${li - translate}`);
-        }
-      });
-      return infected;
-    }, [] as string[])
-    .reduce((grid, infected) => ({...grid, [infected]: true}), {});
+  const result: any = {};
+  lines.forEach((line, li) => {
+    line.split('').forEach((c, ci) => {
+      if (c === '#') {
+        result[`${ci - translate},${li - translate}`] = true;
+      }
+      if (c === '=') {
+        result[`${ci - translate},${li - translate}`] = false;
+      }
+    });
+  });
+  return result;
 };
 
 // export const get = (tubes: ReadonlyArray<string>, [line, col]: Pair<number>) =>
 //   line >= 0 && line < tubes.length && col >= 0 && col < tubes[line].length ? tubes[line][col] : VOID;
 
-export const turn = (from: Direction, left: boolean): Direction => {
-  const current = TURN_LEFT.indexOf(from);
-  const next = left ?
-    (current + 1) % TURN_LEFT.length :
-    current === 0 ? TURN_LEFT.length - 1 : current - 1;
-  return TURN_LEFT[next];
+
+export const countInfectingBursts = (initial: Grid, iterations: number): number => {
+  let i = 0;
+  let infection: Infection = {
+    grid: initial, direction: Direction.up, position: [0,0]
+  };
+  let infecting = 0;
+  while (i < iterations) {
+    const currentPos = infection.position.join(',');
+    const wasInfected = infection.grid[currentPos];
+    infection = burst(infection);
+    if (!wasInfected && infection.grid[currentPos]) {
+      infecting++;
+    }
+    i++;
+  }
+  return infecting;
 };
 
 export const main = (input: string) => {
-  // const tubes = splitLines(input);
-  // const letters: string[] = [];
-  // let steps = 1; // > (including the first line at the top of the diagram)
-  // let pos: Pair<number> = [0, tubes[0].indexOf(Tubes.UP_DOWN)];
-  // let dir: Direction | undefined = Direction.down;
-  //
-  // do {
-  //   const nextCorner = walkToNextCorner(tubes, pos, dir, letters);
-  //   dir = turn(tubes, nextCorner, dir);
-  //
-  //   const [lineBefore, colBefore] = pos; // keep old values before overwriting
-  //   const [line, col] = nextCorner;
-  //   steps += Math.abs(line - lineBefore) + Math.abs(col - colBefore);
-  //
-  //   pos = nextCorner;
-  // } while (dir);
-  return;// [letters.join(''), steps];
+  const initial = parseGrid(splitLines(input));
+
+  return countInfectingBursts(initial, 10000);
 };
 
 if (require.main === module) runWithStdIn(main);
